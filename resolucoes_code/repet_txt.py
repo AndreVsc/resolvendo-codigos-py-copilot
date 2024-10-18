b16f153f747a812b4e79f7a8f71cd645f3b737d7
André Victor Soares Castro <95365578+AndreVsc@users.noreply.github.com>
2024-10-18 07:02:45 -0300

    Update repet_txt.py

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,13 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+num = []
+soma = 0
+
+# Solicitar dois números do usuário
+for i in range(2):
+    numero = int(input("Digite um número: "))
+    print("\n")
+    num.append(numero)
+    subtracao -= numero
+
+print("\n")
+print("O resultado da subtração entre esses números é: " + str(subtracao))
