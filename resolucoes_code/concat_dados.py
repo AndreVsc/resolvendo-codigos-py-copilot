455d8fcf8f29d7938c282407fc3ba33329e7ce0e
André Victor Soares Castro <95365578+AndreVsc@users.noreply.github.com>
2024-10-18 06:55:02 -0300

    done concat

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,6 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+nome = input("Escreva seu nome: ")
+print("\n")
+sobrenome = input("Escreva seu sobrenome: ")
+print("\n")
+print(nome + " " + sobrenome)
